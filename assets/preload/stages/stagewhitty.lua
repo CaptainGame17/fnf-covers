1cadc741f12c45ea55f163a9ca207bbf6e460f7f
FunnyCaptain <40477326+CaptainGame17@users.noreply.github.com>
2021-10-24 21:52:32 -0500

    some lil details

--- assets/preload/stages/stagewhitty.lua
+++ assets/preload/stages/stagewhitty.lua
@@ -9,25 +9,25 @@ function onCreate()
 
 	-- sprites that only load if Low Quality is turned off
 	if not lowQuality then
-		makeLuaSprite('stagelight_left', 'stage_light', -125, -100);
-		setScrollFactor('stagelight_left', 0.9, 0.9);
-		scaleObject('stagelight_left', 1.1, 1.1);
+		makeLuaSprite('blank', 'blank', -125, -100);
+		setScrollFactor('blank', 0.9, 0.9);
+		scaleObject('blank', 1.1, 1.1);
 		
-		makeLuaSprite('stagelight_right', 'stage_light', 1225, -100);
-		setScrollFactor('stagelight_right', 0.9, 0.9);
-		scaleObject('stagelight_right', 1.1, 1.1);
-		setProperty('stagelight_right.flipX', true); --mirror sprite horizontally
+		makeLuaSprite('blank', 'blank', 1225, -100);
+		setScrollFactor('blank', 0.9, 0.9);
+		scaleObject('blank', 1.1, 1.1);
+		setProperty('blank.flipX', true); --mirror sprite horizontally
 
-		makeLuaSprite('stagecurtains', 'stagecurtains', -500, -300);
-		setScrollFactor('stagecurtains', 1.3, 1.3);
-		scaleObject('stagecurtains', 0.9, 0.9);
+		makeLuaSprite('blank', 'blank', -500, -300);
+		setScrollFactor('blank', 1.3, 1.3);
+		scaleObject('blank', 0.9, 0.9);
 	end
 
 	addLuaSprite('whittyBack', false);
 	addLuaSprite('whittyFront', false);
-	addLuaSprite('stagelight_left', false);
-	addLuaSprite('stagelight_right', false);
-	addLuaSprite('stagecurtains', false);
+	addLuaSprite('blank', false);
+	addLuaSprite('blank', false);
+	addLuaSprite('blank', false);
 	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
 end
