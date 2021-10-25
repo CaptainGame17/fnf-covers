ba1028befd5559d1d34dac088d62e20f7b1755d4
FunnyCaptain <40477326+CaptainGame17@users.noreply.github.com>
2021-10-24 19:03:30 -0500

    added the characters and creidts
    now i just gotta redo the whole damn cover cuz the midi i used fucking sucked

---
+++ mods/whitty/stages/stagewhitty.lua
@@ -0,0 +1,33 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('whittyBack', 'whittyBack', -600, -300);
+	setScrollFactor('whittyBack', 0.9, 0.9);
+	
+	makeLuaSprite('whittyFront', 'whittyFront', -650, 600);
+	setScrollFactor('whittyFront', 0.9, 0.9);
+	scaleObject('whittyFront', 1.1, 1.1);
+
+	-- sprites that only load if Low Quality is turned off
+	if not lowQuality then
+		makeLuaSprite('stagelight_left', 'stage_light', -125, -100);
+		setScrollFactor('stagelight_left', 0.9, 0.9);
+		scaleObject('stagelight_left', 1.1, 1.1);
+		
+		makeLuaSprite('stagelight_right', 'stage_light', 1225, -100);
+		setScrollFactor('stagelight_right', 0.9, 0.9);
+		scaleObject('stagelight_right', 1.1, 1.1);
+		setProperty('stagelight_right.flipX', true); --mirror sprite horizontally
+
+		makeLuaSprite('stagecurtains', 'stagecurtains', -500, -300);
+		setScrollFactor('stagecurtains', 1.3, 1.3);
+		scaleObject('stagecurtains', 0.9, 0.9);
+	end
+
+	addLuaSprite('whittyBack', false);
+	addLuaSprite('whittyFront', false);
+	addLuaSprite('stagelight_left', false);
+	addLuaSprite('stagelight_right', false);
+	addLuaSprite('stagecurtains', false);
+	
+	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+end
--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -22,26 +22,32 @@ class CreditsState extends MusicBeatState
 
 	private var grpOptions:FlxTypedGroup<Alphabet>;
 	private var iconArray:Array<AttachedSprite> = [];
- // the reason half the credits are like that is because i had to make a normal ass dadbattle cover with no bf character changes cuz the engine got really pissed off at me
+ 
 	private static var creditsStuff:Array<Dynamic> = [ //Name - Icon name - Description - Link - BG Color
 		['Everyone Sings Team'],
 		['Captain',		'captain',		'Cover Creator & Mod Maker',					'https://www.youtube.com/channel/UCStTKMMLms3GBxhJVsSsAWg',	0xFF8AB6D4],
 		[''],
 		['Original Mod Teams'],
-	 //	['Friday Night Funkin\' D-Sides Team',		'dsides',		'Funky Remixes',					'https://gamebanana.com/mods/305122',	0xFFE455D8],
+		['A.G.O.T.I. Team',		'agoti',		'A.G.O.T.I.',					'https://gamebanana.com/mods/284934',	0xFFBA1E24],
+		['Arcade Showdown Team',		'kapi',		'Kapi & Mr. Game and Watch',					'https://gamebanana.com/mods/44683',	0xFF747585],
+		['Back Alley Blitz Team',		'whitty',		'Whitty & Updike',					'https://c.tenor.com/bDln6hWwCe0AAAAd/fnf-friday-night-funkin.gif',	0xFF1D1E35],
+		['Bob and Bosip Team',		'bobandbosip',		'Bob, Bosip, Amor...',					'https://gamebanana.com/mods/297087',	0xFF3A68FF],
+		['Carol Team',		'carol',		'Carol',					'https://gamebanana.com/mods/42811',	0xFF666699], // nice 2
+		['Friday Night Fever Team',		'fridaynightfever',		'CesarFever, Taki, Tea...',					'https://gamebanana.com/mods/310465',	0xFFC353E3],
+		['Nonsense Team',		'nonsense',		'Nonsense',					'https://gamebanana.com/mods/292607',	0xFF74E7FF],
+		['Pinkie Team',		'pinkie',		'Pinkie Pie',					'https://drive.google.com/file/d/1TjLTgFItJ_-qSIvl0d3f4_wPiZaQBtev/view?usp=sharing',	0xFFFFFF99],
+		['Shaggy Team',		'shaggy',		'Various Shaggys',					'https://gamejolt.com/games/fnf-shaggy/643999',	0xFF33724A],
+		['Smoke \'Em Out Struggle Team',		'garcello',		'Garcello',					'https://gamebanana.com/mods/166531',	0xFF00FF90],
+		['Sunday Team',		'sunday',		'Sunday',					'https://gamebanana.com/mods/300849',	0xFF669999], // nice
 		['The Full-Ass Tricky Mod Team',		'tricky',		'Tricky',					'https://gamebanana.com/mods/44334',	0xFF185F40],
 		['literally every fnf mod team ever',		'bobsonslaught',		'Bob, Ron, & Little Man',					'https://gamebanana.com/mods/285296',	0xFFFFFFFF],
-		['Bob and Bosip Team',		'bobandbosip',		'Bob, Bosip, Amor...',					'https://gamebanana.com/mods/297087',	0xFF3A68FF],
-		['A.G.O.T.I. Team',		'agoti',		'A.G.O.T.I.',					'https://gamebanana.com/mods/284934',	0xFFBA1E24],
-	 //	['Sonic.EXE Team',		'sonicexe',		'Sonic.EXE, Majin Sonic, Sunky.MPEG',					'https://gamebanana.com/mods/316022',	0xFF0058B7],
-	 //	['Tord Team',		'tord',		'Tord',					'https://gamebanana.com/mods/44406',	0xFFCC3333],
-		['Sunday Team',		'sunday',		'Sunday',					'https://gamebanana.com/mods/300849',	0xFF669999], // nice
-		['Friday Night Fever Team',		'fridaynightfever',		'CesarFever, Taki, Tea...',					'https://gamebanana.com/mods/310465',	0xFFC353E3],
+	 	['Sonic.EXE Team',		'sonicexe',		'Sonic.EXE, Majin Sonic, Sunky.MPEG',					'https://gamebanana.com/mods/316022',	0xFF0058B7],
+	 	['Tabi Team',		'tabi',		'Tabi',					'https://gamebanana.com/mods/286388',	0xFFFFBB81],
+	 	['Tord Team',		'tord',		'Tord',					'https://gamebanana.com/mods/44406',	0xFFCC3333],
 	 //	['FNF: DustTale Remastered Team',		'dusttale',		'Murder!Sans, Phantom!Papyrus...',					'https://gamebanana.com/mods/287084',	0xFF999999],
-	 //	['Vs Tabi Team',		'tabi',		'Tabi',					'https://gamebanana.com/mods/286388',	0xFFFFBB81],
-		['Smoke \'Em Out Struggle Team',		'garcello',		'Garcello',					'https://gamebanana.com/mods/166531',	0xFF00FF90],
-	 //	['VS Impostor Team',		'impostor',		'A bunch of sussy bakas',					'https://gamebanana.com/mods/55652',	0xFFFF3333],
+	 //	['Friday Night Funkin\' D-Sides Team',		'dsides',		'Funky Remixes',					'https://gamebanana.com/mods/305122',	0xFFE455D8],
 	 //	['HEARTBREAK: Vs Girlfriend Team',		'vsgirlfriend',		'Girlfriend',					'https://gamebanana.com/mods/46716',	0xFFA5004D],
+	 //	['Impostor Team',		'impostor',		'A bunch of sussy bakas',					'https://gamebanana.com/mods/55652',	0xFFFF3333],
 		['Psych Engine Team'],
 		['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',					'https://twitter.com/Shadow_Mario_',	0xFFFFDD33],
 		['RiverOaken',			'riveroaken',		'Main Artist/Animator of Psych Engine',				'https://twitter.com/river_oaken',		0xFFC30085],
